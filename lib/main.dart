e850bfff3ef27dabc755f3a3ed148e1877ace730
huanggengzhong <915835528@qq.com>
2019-11-18 14:37:38 +0800

    登录页面完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:heima_app/login/login.dart';
+import 'package:heima_app/home/home.dart';
+import 'package:heima_app/moudle/pub.dart';
 
 void main() {
-  runApp(App());
+  PubMoudle.isLogin().then((value) {
+    runApp(App(value));
+  });
 }
 
 class App extends StatelessWidget {
+  final String token;
+  App(this.token);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'黑马头条',//记得MaterialApp的title是无需Text组件的.
-      home: LoginPage(),
-      debugShowCheckedModeBanner: false//去掉右上角debug
-      );
+        title: '黑马头条', //记得MaterialApp的title是无需Text组件的.
+        // home: token!=null?Home(): LoginPage(),//有token去主页,没有去登录页,因为测试,先不用
+        home:LoginPage(),
+        debugShowCheckedModeBanner: false, //去掉右上角debug
+        routes: {"/home": (context) => Home()});
   }
 }
